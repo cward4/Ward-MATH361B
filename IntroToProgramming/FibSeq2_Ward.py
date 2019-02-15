629d341e5f278e0b0b2f01f06380b4cbe5e297d4
cward4 <cward4@mail.umw.edu>
2019-02-15 12:47:22 -0500

    I.6 Fibonacci Sequence 2 (updated)

--- IntroToProgramming/FibSeq2_Ward.py
+++ IntroToProgramming/FibSeq2_Ward.py
@@ -4,32 +4,18 @@ Created on Wed Feb 13 12:28:55 2019
 
 @author: caitl
 """
-
+N = 10000
+m = 2
+first_terms = [0,1]
 list = []
-evens = []
-mult_three =[]
-mult_four =[]
-prime = []
-F_0 = 0
-F_1 = 1
-list.append(F_0)
-list.append(F_1)
-for i in range(2,10000):
-    list.append((list[(i-1)]) + (list[(i-2)]))
-for i in range(len(list)):
-    if (((list[i]) % 2) == 0):
-        evens.append(list[i])
-    if (((list[i]) % 3) == 0):
-        mult_three.append(list[i])
-    if (((list[i]) % 4) == 0):
-        mult_four.append(list[i]) 
-#for ii in range(2,list[i]):
- #   if (list[i] >1 and list[i] % ii == 0):
-  #      prime.append(list[i]) 
 
-#print(len(evens))
-print((len(evens)/10000)*100)
-print((len(mult_three)/10000)*100)
-print((len(mult_four)/10000)*100)
+for i in range(2,N):
+    f = first_terms[i-1]+first_terms[i-2]
+    first_terms.append(f)
+    if f%m==0:
+        list.append(f)
 
-#print(len(prime))
+length = len(list)
+percentage = (length/N)
+
+print('Percentage of ', m, 'multiples is ', percentage)
